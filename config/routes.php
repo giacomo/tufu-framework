c77eaae83fb289f2bb1207f2e95b03c11797de67
Giacomo Barbalinardo <info@ready24it.eu>
2022-07-04 08:08:14 +0200

    added: added preflight routes

--- config/routes.php
+++ config/routes.php
@@ -10,6 +10,7 @@ use Tufu\Core\RouteManager;
 $routesManager = RouteManager::getInstance();
 
 $routesManager->addGetRoute('/', 'App\Controller\Home@welcomeAction');
+$routesManager->addOptionsRoute('/api/{routes}', 'App\Controller\Preflight@optionsAction');
 $routesManager->addGetRoute('/api/welcome', 'App\Controller\Api@indexAction');
 $routesManager->addPostRoute('/api/login', 'App\Controller\Auth@loginAction');
 $routesManager->addGetRoute('/api/protected', 'App\Controller\Api@protectedAction');
